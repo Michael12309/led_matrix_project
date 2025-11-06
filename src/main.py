7acbcda02f098d5ed19a9249522c8480ec57f457
Michael Leonard <michaelleonard5555@gmail.com>
2025-11-06 13:16:34 -0500

    update

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     workout_split_seed = 1
 
     # Start date: October 2, 2025
-    start_date = datetime.datetime(2025, 10, 1, 0, 0, 0)
+    start_date = datetime.datetime(2025, 10, 2, 0, 0, 0)
 
 
     while True:
@@ -36,18 +36,18 @@ if __name__ == "__main__":
             elapsed = current_time - start_date
             
             total_seconds = int(elapsed.total_seconds())
-            days = elapsed.days
-            weeks = days // 7
-            months = days // 30
+            day = (elapsed.days) + 1
+            week = (day // 7) + 1
+            month = (day // 30) + 1
             
             time_str = current_time.strftime('%I:%M:%S %p')
 
             # Display
-            matrix.drawText(f"Day {days}", "medium", (center_text(f"Day {days}"), 10), (178, 34, 52))
-            matrix.drawText(f"Week {weeks}", "medium", (center_text(f"Week {weeks}"), 20), (255, 255, 255))
-            matrix.drawText(f"Month {months}", "medium", (center_text(f"Month {months}"), 30), (60, 59, 110))
-            matrix.drawText(f"{workout_split[(days + workout_split_seed) % len(workout_split)]}", "medium", (center_text(workout_split[(days + workout_split_seed) % len(workout_split)]), 50), (255, 255, 255))
-            matrix.drawText(f"{time_str}", "medium", (center_text(time_str), 60), (180, 180, 180))
+            matrix.drawText(f"Day {day}", "medium", (center_text(f"Day {day}"), 10), (178, 34, 52))
+            matrix.drawText(f"Week {week}", "medium", (center_text(f"Week {week}"), 20), (255, 255, 255))
+            matrix.drawText(f"Month {month}", "medium", (center_text(f"Month {month}"), 30), (60, 59, 110))
+            matrix.drawText(f"{workout_split[(day + workout_split_seed) % len(workout_split)]}", "medium", (center_text(workout_split[(day + workout_split_seed) % len(workout_split)]), 50), (255, 255, 255))
+            matrix.drawText(f"{time_str}", "medium", (center_text(time_str), 60), (160, 160, 160))
             
         except Exception as e:
             print(f"Exception: {e}")
